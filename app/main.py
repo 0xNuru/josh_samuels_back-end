07ab979642f7f3e65d56f6de9ac60094333f1145
nuru <0xnuru@gmail.com>
2024-08-13 15:54:55 +0100

    feat: add auth and auth router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
-from app.routers import customer
+from app.routers import customer, auth
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500", "http://localhost:5501", "http://127.0.0.1:5502"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def hello():
     return {"message": "Hello, World!"}
 
 app.include_router(customer.router)
+app.include_router(auth.router)
