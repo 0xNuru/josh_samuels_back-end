ac9dc124881095b75c3d6b396d7791b5c307b3bf
nuru <0xnuru@gmail.com>
2024-08-23 11:57:31 +0100

    chore: add joshsamuels to allowed domains in cors

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://localhost:5501", "http://127.0.0.1:5502"],
+    allow_origins=["https://joshsamuels.co", "https://www.joshsamuels.co"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
