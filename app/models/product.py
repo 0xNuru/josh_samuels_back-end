23b4d85494d0b096f935053ae82142b9609c5ea9
nuru <0xnuru@gmail.com>
2024-08-25 15:14:09 +0100

    feat: rm stock quantity from product model and add images column as a list of strings

--- app/models/product.py
+++ app/models/product.py
@@ -1,7 +1,11 @@
 from sqlalchemy import Column, Float, Integer, LargeBinary, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
 
 from app.models.base_model import BaseModel, Base
+
+
+DEFAULT_STOCK_IMAGE_URL = "https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
 
 
 class Product(BaseModel, Base):
@@ -10,8 +14,6 @@ class Product(BaseModel, Base):
     price = Column(Float, nullable=False)
     description = Column(String, nullable=True)
     category = Column(String, nullable=True)
-    stock_quantity = Column(Integer, default=0)
-    product_image = Column(LargeBinary, nullable=True)
-    product_image_header = Column(String, nullable=True)
+    images = Column(JSONB, nullable=True, default=lambda: [DEFAULT_STOCK_IMAGE_URL])
 
     cart = relationship("Cart", back_populates="product")
