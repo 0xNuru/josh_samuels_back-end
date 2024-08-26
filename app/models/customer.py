e3f0b147efe3bbb551fa3da14dc593b8a544a7cf
nuru <0xnuru@gmail.com>
2024-08-26 22:08:09 +0100

    chore: make dob a string but with pydantic field validator

--- app/models/customer.py
+++ app/models/customer.py
@@ -2,7 +2,8 @@
 
 import enum
 
-from sqlalchemy import Column, Date, Enum, ForeignKey, String, LargeBinary
+from sqlalchemy import Column, Enum, ForeignKey, String, LargeBinary
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import relationship
 
 from app.models.user import User
@@ -20,11 +21,9 @@ class Customer(User):
     id = Column(String, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     first_name: str = Column(String(128), nullable=False)
     last_name: str = Column(String(128), nullable=False)
-    date_of_birth = Column(Date, nullable=True)
+    date_of_birth = Column(String, nullable=True)
     gender = Column(Enum(GenderEnum, name="genders"), nullable=True)
     address: str = Column(String(256), nullable=True)
-    image = Column(LargeBinary, nullable=True)
-    image_header = Column(String, nullable=True)
 
     measurement = relationship("Measurement", back_populates="customer")
     cart = relationship("Cart", back_populates="customer")
