4e658d510e8fc2e98d83a79d754bcde5f667e026
nuru <0xnuru@gmail.com>
2024-08-28 06:47:04 +0100

    feat: add paystack webhood endpoint

--- app/routers/payment.py
+++ app/routers/payment.py
@@ -15,3 +15,16 @@ def initialize_payment(request: Payments):
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid request"
         )
     return {"access_code": access_code}
+
+
+@router.post("/paystack_webhook")
+async def paystack_webhook(request):
+    # if request.event == "charge.success":
+    #     payment_data = request.data
+    # Do something with payment data
+    # Example: Save payment data to database, send email to customer, update order status, etc.
+    #     return {"message": "Payment successful"}  # redirect to a payment succesful page
+    # return {
+    #     "message": "Payment failed"
+    # }
+    print(request)
